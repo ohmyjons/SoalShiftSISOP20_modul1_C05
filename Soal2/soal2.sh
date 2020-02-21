cdcac9f2985e1fe9dbe7a6eba40f498820b0b65c
ohmyjons <dohanpw@gmail.com>
2020-02-21 07:18:07 +0700

    harus .txt

--- Soal2/soal2.sh
+++ Soal2/soal2.sh
@@ -5,7 +5,7 @@ jam=`date +"%H"`
 
 file=$1
 
-fix=$(echo "$file" | tr -d '.txt')
+fix=$(echo "$file" | grep '.*(?=\.txt)'| tr -d '.txt' | tr -dc A-Za-z )
 
 besar=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
 kecil=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
