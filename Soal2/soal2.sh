4d1f11d2587d99f361b8db3d3e2de084e4323475
ohmyjons <dohanpw@gmail.com>
2020-02-21 07:42:11 +0700

    Real fix harus .TXT

--- Soal2/soal2.sh
+++ Soal2/soal2.sh
@@ -5,7 +5,7 @@ jam=`date +"%H"`
 
 file=$1
 
-fix=$(echo "$file" | grep '.*(?=\.txt)'| tr -d '.txt' | tr -dc A-Za-z )
+fix=$(echo "$file" | grep -P '.*(?=\.txt)'| tr -d '.txt' | tr -dc A-Za-z )
 
 besar=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
 kecil=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
@@ -18,7 +18,7 @@ cek4=${kecil[jam-1]}
 # echo $cek2
 # echo $cek4
 
-# asu2=$(echo "ABCD EFG" | tr '[A-Z]' '[(${besar[n]})-ZA-([${besar[n]}])]')
+# asus=$(echo "ABCD EFG" | tr '[A-Z]' '[(${besar[n]})-ZA-([${besar[n]}])]')
 
 save=$(echo "$fix" | tr '[A-Za-z]' ["$cek1"-ZA-"$cek2""$cek3"-za-"$cek4"])
 # echo ${besar[n]}
